695f114dbe8bfcc047b3fedef7911f57dfcb60dd
Iamsandwich <landauliza@gmail.com>
2026-07-01 01:26:53 +0300

    added train and fireplace!

--- effects.js
+++ effects.js
@@ -1,9 +1,13 @@
 const ESlider = document.getElementById("slidereffects");
 var Eaudio = document.getElementById("waves");
 var Eaudio2 = document.getElementById("rain");
+var Eaudio3 = document.getElementById("train");
+var Eaudio4 = document.getElementById("fireplace");
 
 console.log(ESlider);
 console.log(Eaudio2);
+console.log(Eaudio3);
+console.log(Eaudio4);
 console.log(Eaudio);
 
 
@@ -11,5 +15,7 @@ console.log(Eaudio);
 ESlider.addEventListener("change", function(){
     Eaudio.volume = ESlider.value;
     Eaudio2.volume = ESlider.value;
+    Eaudio3.volume = ESlider.value;
+    Eaudio4.volume = ESlider.value;
     console.log(ESlider.value);
 });
